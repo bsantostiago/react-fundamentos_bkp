6650e4bff6ac537642b1bdf4bb5bdbc5ca599651
Tiago Bezerra dos Santos <bsantostiago@gmail.com>
2023-11-30 22:02:54 -0300

    StyledArtigo

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,10 +1,18 @@
+import styled from "styled-components";
+
+const StyledArtigo = styled.article`
+  background-color: lavender;
+  padding: 1rem;
+  margin: 0.5rem 0;
+`;
+
 function Artigo() {
   return (
     <>
-      <article>
+      <StyledArtigo>
         <h3>Artigo 1</h3>
         <p>Conteúdo do artigo 1....</p>
-      </article>
+      </StyledArtigo>
     </>
   );
 }
