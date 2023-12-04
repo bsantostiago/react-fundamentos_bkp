021a393147ce763977f2d9506ca92b4544cc55c0
Tiago Bezerra dos Santos <bsantostiago@gmail.com>
2023-12-04 20:49:29 -0300

    Update Artigo.jsx

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,21 +1,5 @@
 import styled from "styled-components";
 
-const StyledArtigo = styled.article`
-  background-color: lavender;
-  padding: 1rem;
-  margin: 0.5rem 0;
-
-  h3,
-  p {
-    padding: 0.5rem 0;
-  }
-
-  h3 {
-    color: purple;
-    text-align: center;
-  }
-`;
-
 function Artigo({ titulo, categoria, preco }) {
   const formataPreco = (valor) => {
     return valor.toLocaleString("pt-br", {
@@ -34,23 +18,21 @@ function Artigo({ titulo, categoria, preco }) {
     </StyledArtigo>
   );
 }
-/* function Artigo(props) {
-  return (
-    <StyledArtigo>
-      <h3>
-        {props.titulo} {props.icone}
-      </h3>
-      {props.children}
 
-      <footer>
-        <p>
-          <small>
-            <i>Responsável: {props.responsavel}</i>
-          </small>
-        </p>
-      </footer>
-    </StyledArtigo>
-  );
-}
- */
+const StyledArtigo = styled.article`
+  background-color: lavender;
+  padding: 1rem;
+  margin: 0.5rem 0;
+
+  h3,
+  p {
+    padding: 0.5rem 0;
+  }
+
+  h3 {
+    color: purple;
+    text-align: center;
+  }
+`;
+
 export default Artigo;
