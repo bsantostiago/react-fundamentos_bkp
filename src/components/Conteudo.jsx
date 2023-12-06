def1dc996e80d8d1c2790f509e4c29c43a803f4a
Tiago Bezerra dos Santos <bsantostiago@gmail.com>
2023-12-06 08:59:06 -0300

    .

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -2,6 +2,43 @@ import styled from "styled-components";
 import Artigo from "./Artigo";
 import cursos from "../api/cursos";
 
+function Conteudo() {
+  const exemplo2 = () => {
+    alert("Exemplo 2");
+  };
+
+  /* Também usado do filho pro pai */
+  function exemplo3(valor) {
+    alert(`Exemplo 3: ${valor}`);
+  }
+
+  return (
+    <StyledConteudo>
+      <h2>Conteúdo da aplicação</h2>
+      <button onClick={exemplo2}>Exemplo 2: evento/função</button>
+      <div className="artigos">
+        {cursos.map((curso) => {
+          return (
+            <Artigo
+              // onClick={exemplo3} // não dá
+              /* 1) Passamos a função como uma propriedade para o componente-filho: */
+              // onClick={() => exemplo3(curso.titulo)} // não dá
+              // handleClick={() => exemplo3(curso.titulo)}
+              aoClicar={() => exemplo3(curso.titulo)}
+              key={curso.id}
+              titulo={curso.titulo}
+              categoria={curso.categoria}
+              preco={curso.preco}
+            >
+              <p>coisa e tal....</p>
+            </Artigo>
+          );
+        })}
+      </div>
+    </StyledConteudo>
+  );
+}
+
 const StyledConteudo = styled.main`
   width: 90vw;
   max-width: 800px;
@@ -39,44 +76,5 @@ const StyledConteudo = styled.main`
     }
   }
 `;
-
-function Conteudo() {
-  const exemplo2 = () => {
-    alert("Exemplo 2");
-  };
-
-  /* Também usado do filho pro pai */
-  function exemplo3(valor) {
-    alert(`Exemplo 3: ${valor}`);
-  }
-
-  return (
-    <StyledConteudo>
-      <h2>Conteúdo da aplicação</h2>
-
-      <button onClick={exemplo2}>Exemplo 2: evento/função</button>
-
-      <div className="artigos">
-        {cursos.map((curso) => {
-          return (
-            <Artigo
-              // onClick={exemplo3} // não dá
-              /* 1) Passamos a função como uma propriedade para o componente-filho: */
-              // onClick={() => exemplo3(curso.titulo)} // não dá
-              // handleClick={() => exemplo3(curso.titulo)}
-              aoClicar={() => exemplo3(curso.titulo)}
-              key={curso.id}
-              titulo={curso.titulo}
-              categoria={curso.categoria}
-              preco={curso.preco}
-            >
-              <p>coisa e tal....</p>
-            </Artigo>
-          );
-        })}
-      </div>
-    </StyledConteudo>
-  );
-}
 
 export default Conteudo;
